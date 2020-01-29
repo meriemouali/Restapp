477297a8c6eb9f21a01d6fa3fd767be4ef391fa1
MariemOuali <mariem.ouali@target-energysolutions.com>
2020-01-29 10:33:54 +0100

    first commit

---
+++ src/main/java/com/example/RestApp/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.RestApp;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/")
+    public String index(){
+        return "helloworld";
+    }
+}
